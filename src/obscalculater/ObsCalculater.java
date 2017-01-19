8fd329c5879423f14a7350921cf0c86f3754ecda
MERC-LAB-01 <MERC-LAB-01@MERC-LAB-18>
2017-01-19 14:25:36 +0530

    first version

---
+++ src/obscalculater/ObsCalculater.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package obscalculater;
+
+/**
+ *
+ * @author MERC-LAB-01
+ */
+public class ObsCalculater {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ObsJFrame obsCalculater = new ObsJFrame();
+        obsCalculater.setVisible(true);
+    }
+    
+}
